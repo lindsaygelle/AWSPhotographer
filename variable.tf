69b8c49d09dbe2e01c42e43541a8e99090b5bc16
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-08-31 17:08:27 +1000

    Dev (#4)
    * Dev

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "application" {
+  default   = "photographer"
+  sensitive = false
+  type      = string
+}
+
+variable "region" {
+  default   = "us-east-1"
+  sensitive = false
+  type      = string
+}
