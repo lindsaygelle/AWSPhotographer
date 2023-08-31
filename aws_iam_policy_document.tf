70c5d6cae82cf5d2f020ed6a84fd74578f39cf71
lindsaygelle <lindsay.gelle@gmail.com>
2023-08-31 17:19:34 +1000

    Dev

---
+++ aws_iam_policy_document.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "s3_bucket_notification_s3_object_images_uploaded" {
+  statement {
+    actions = [
+      "SNS:Publish"
+    ]
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "s3.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    resources = [
+      aws_sns_topic.s3_object_images_uploaded.arn
+    ]
+  }
+}
---
+++ aws_s3_bucket_notification.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket_notification" "images_uploaded" {
+  bucket = aws_s3_bucket.main.id
+  topic {
+    events = [
+      "s3:ObjectCreated:*"
+    ]
+    filter_prefix = aws_s3_object.images_uploaded.key
+    topic_arn     = aws_sns_topic.s3_object_images_uploaded.arn
+  }
+}
